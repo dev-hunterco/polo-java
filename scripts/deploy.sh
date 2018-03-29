ddbaed4d71434426970f63a98649f0a642e50f9d
Flavio Cordova <flavio.cordova@gmail.com>
2018-03-28 23:09:19 -0300

    still publishing file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-TARGET_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version |grep -Ev '(^\[|Download\w+:)'`
-TARGET_VERSION=${TARGET_VERSION/-SNAPSHOT/}
-
-echo Maven Base Version: $TARGET_VERSION
-echo Current Tag: $TRAVIS_TAG
 
 if [ "$1" = 'production' ] && [ "$TARGET_VERSION" = "$TRAVIS_TAG"]; then
     echo ----------------------------------------------------------
