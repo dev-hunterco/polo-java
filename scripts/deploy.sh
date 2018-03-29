1e5c607b22aa9b630d08978c6265b33ff3e40144
Flavio Cordova <flavio.cordova@gmail.com>
2018-03-28 22:57:58 -0300

    trying to build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+TARGET_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version |grep -Ev '(^\[|Download\w+:)'`
+TARGET_VERSION=${TARGET_VERSION/-SNAPSHOT/}
 
-if [ "$1" = 'production' ]; then
+echo Maven Base Version: $TARGET_VERSION
+echo Current Tag: $TRAVIS_TAG
+
+if [ "$1" = 'production' ] && [ "$TARGET_VERSION" = "$TRAVIS_TAG"]; then
     echo ----------------------------------------------------------
     echo ------------------ DEPLOYING PRODUCTION ------------------
     echo ----------------------------------------------------------
-    
 else
     echo ----------------------------------------------------------
     echo ------------------ DEPLOYING SNAPSHOP --------------------
     echo ----------------------------------------------------------
 fi
 
+echo Building version...
 # mvn deploy -P release --settings settings.xml
