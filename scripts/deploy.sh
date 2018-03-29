f5f86cf814f01fbff028f8ba16e6192068dbb2e2
Flavio Cordova <flavio.cordova@gmail.com>
2018-03-29 10:05:37 -0300

    production release

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,14 @@
 
 if [ "$1" = 'production' ] && [ "$TARGET_VERSION" = "$TRAVIS_TAG"]; then
     echo ----------------------------------------------------------
-    echo -------- DEPLOYING PRODUCTION - $TARGET_VERSION-----------
+    echo -------- DEPLOYING PRODUCTION - $TARGET_VERSION ----------
     echo ----------------------------------------------------------
+    echo Changing pom.xml version...
+    mvn versions:set -DnewVersion=$TARGET_VERSION
 else
     echo ----------------------------------------------------------
     echo ----------- DEPLOYING SNAPSHOP - $POM_VERSION ------------
     echo ----------------------------------------------------------
 fi
 
-echo Deploying version...
-mvn deploy -Dmaven.test.skip=true -P release --settings settings.xml
+mvn deploy -B -Dmaven.test.skip=true -P release --settings settings.xml
