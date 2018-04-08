e5b570ea522b549980f267381bf671dbbfebd37d
Flavio Cordova <flavio.cordova@gmail.com>
2018-04-08 18:23:11 -0300

    Extract version from POM when deploying...

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+echo "Inspecting version..."
+# Inspect twice - first to download all the dependencies, then really get the version
+mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version> /dev/null
+export POM_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep "^[0-9]" | grep SNAPSHOT$` 
+export TARGET_VERSION=${POM_VERSION/-SNAPSHOT/}
+
 echo Starting deploying...
 echo Mode: $1
 echo Travis Tag: "$TRAVIS_TAG"
 echo Target Version: $TARGET_VERSION
+
 
 if [ "$1" = "production" ] && [ "$TARGET_VERSION" = "$TRAVIS_TAG"]; then
     echo ----------------------------------------------------------
